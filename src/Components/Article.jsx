20ef5d8f907bcfa10424a8db5e67af6870c00a0e
PLATYPUSCO007 <william.enciso@cun.edu.co>
2024-06-12 18:40:25 -0500

    List articles and detail init

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,18 +1,64 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export const Article = () => {
 
-  useEffect(async ()=>{
-    try {
-      const petitionData =  fetch('')
-    } catch (error) {
-     console.error(`Error en la peticion: ${error}`); 
-     throw new error(error);
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    async function fetchData(){
+      try {
+        const petitionData = await fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=50');
+        const data =  await petitionData.json();
+        setProducts(data.slice(0, 40));
+      } catch (error) {
+        console.error(`Error en la peticion: ${error}`); 
+        throw new error(error);
+      }
     }
+    fetchData();
   },
   [])
 
   return (
-    <div>Article</div>
+    <div className='d-flex flex-row flex-wrap justify-content-center align-items-center'>
+      <div className='d-flex justify-content-center w-100'>
+        <h3>LISTA DE PRODUCTOS</h3>
+      </div>
+      <div className='d-flex flex-row flex-wrap justify-content-evenly w-100'>
+      {
+        (products.length > 0) ? (
+          <>
+          {console.log(products)}
+            {products.map(product=>{
+              return (
+                <div className='d-flex card w-50 m-1' key={product.id}>
+                  <div className='card-header d-flex flex-column justify-content-center align-items-center'>
+                    <h4 className='card-title text-info'>{product.title}</h4>
+                    <img src={product.images[0]} alt="" className='card-image' width="100%" height="300px"/>
+                  </div>
+                  <div className='card-body'>
+                    <h5>Descripción:</h5>
+                    <p>{product.description}</p>
+                    <h5>Precio: <span className='text-danger'>${product.price}</span> </h5>
+                  </div>
+                  <div className='card-footer'>
+                    <h6>Categoria: <span>{(product.category.name) ? product.category.name : ('NA')}</span></h6>
+                    <div className='d-flex justify-content-evenly w-100 mt-3'>
+                      <a href={`/detail/${product.id}`}><button className='btn btn-secondary'> Ver mas <span className='bi bi-plus-circle-dotted'></span></button></a>
+                      <button className='btn btn-primary'>Comprar <span className='bi bi-fire'></span></button>
+                    </div>
+                    <div className='d-flex justify-content-end me-3'>
+                      <span>{product.id}</span>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </>
+        ) : null
+      }
+      </div>
+    </div>
   )
 }
--- src/Components/DetailArticle.jsx
+++ src/Components/DetailArticle.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export const DetailArticle = () => {
+
+  const {id} = useParams();
+  const [product, setProduct] = useState({});
+
+  useEffect(()=>{
+    const petitionFetch = async ()=>{
+      try {
+        const petitionData = await fetch(`https://api.escuelajs.co/api/v1/products/${id}`);
+        const data =  await petitionData.json();
+        setProduct(data);
+      } catch (error) {
+        console.error(`Error en la peticion: ${error}`); 
+        throw new error(error);
+      }
+    }
+    petitionFetch();
+  }, [id])
+
   return (
-    <div>DetailArticle</div>
+    <div className='d-flex flex-wrap w-100'>
+      <div className='d-flex justify-content-center align-items-center w-100'>
+        <h2>Detalle de articulo ID: {id}</h2>
+      </div>
+      {(!product) ? null : (
+        <div className='d-flex justify-content-start w-100'>
+          <div className='d-flex w-50'>
+            <img src={product.images[1]} alt="imagen" width='100%' height='auto'/>
+          </div>
+          <div className='d-flex ms-3 w-50'>
+            <h4>Titulo: {product.title}</h4>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
